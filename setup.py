686152e4d51a571f6ec8c7344a6b4e1fc15bdc6c
Delon Wong Her Laang <wongherlung@gmail.com>
2018-08-08 14:34:11 +0800

    Bump up minor version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zap_report_formatter",
-    version="0.0.1",
+    version="0.0.2",
     author="Wong Her Laang",
     author_email="wongherlung@gmail.com",
     description="Takes in OWASP ZAP json reports and produces whitelisted xml reports for Jenkins Junit plugin.",
