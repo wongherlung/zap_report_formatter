a79330ccb99b687decf65c273347738965463f86
Delon Wong Her Laang <wongherlung@gmail.com>
2018-08-08 14:33:42 +0800

    Fix github repo url.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Takes in OWASP ZAP json reports and produces whitelisted xml reports for Jenkins Junit plugin.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/wongherlung/zap_report_formatter",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2.7",
